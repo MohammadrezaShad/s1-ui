deb44fec5763185c42a2f1904cae5be7390989f8
Javad Hosseinzadeh <javad.hr@gmail.com>
2024-06-27 11:22:53 +0330

    feat: display maintenance message on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import {css} from '@styled/css';
+
 export default function Home() {
-  return <main />;
+  return (
+    <main
+      className={css({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minH: 'screen',
+      })}
+    >
+      <h1
+        className={css({
+          textStyle: 'title1',
+          color: 'grey_800',
+          textAlign: 'center',
+        })}
+      >
+        Our website is undergoing some maintenance.
+        <br />
+        We&apos;ll be back soon.
+      </h1>
+    </main>
+  );
 }
